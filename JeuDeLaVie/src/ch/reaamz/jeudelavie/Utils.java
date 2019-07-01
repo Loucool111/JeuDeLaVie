4d8f90e2d3104bc2b8538cac286cb9f2643532fe
Reaamz <loucool111@gmail.com>
2019-07-01 20:22:13 +0200

    Update

--- JeuDeLaVie/src/ch/reaamz/jeudelavie/Utils.java
+++ JeuDeLaVie/src/ch/reaamz/jeudelavie/Utils.java
@@ -9,7 +9,7 @@ public class Utils
 {
 	public static final String PLUGIN_NAME = "JDLV";
 	
-	public static final Material CLIC_ITEM = Material.WOOD_SWORD;
+	public static final Material CLIC_ITEM = Material.WOODEN_SWORD;
 	public static final Material GAME_BLOCK = Material.LAPIS_BLOCK;
 	public static final Material BASE_BLOCK = Material.STONE;
 	
